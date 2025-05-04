d79ec7a6f0ca4acb5b7b61bfb07f74f5b09e3059
Saravanakumar <sskmscss@yahoo.co.in>
2025-05-04 09:01:34 +0530

    removed /profile

--- image-loader.js
+++ image-loader.js
@@ -3,5 +3,5 @@ module.exports = function (src, width, quality) {
     // For static export, return the original src path
     // Add basePath if you're deploying to a subpath
     const basePath = process.env.NODE_ENV === 'production' ? '/profile' : '';
-    return `/profile${src}`;
+    return `${src}`;
   };
